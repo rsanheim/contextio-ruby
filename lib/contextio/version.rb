525b8f794b8232853e1f62dfb107e6fa51ba52a0
Ryan Michael <kerinin@gmail.com>
2015-10-19 16:03:49 -0500

    Verison bump, test fix

--- lib/contextio/version.rb
+++ lib/contextio/version.rb
@@ -1,6 +1,6 @@
 class ContextIO
   # @private
-  VERSION = "2.0.0"
+  VERSION = "2.0.1"
 
   # The gem version.
   #
--- spec/integration/folders_messages_spec.rb
+++ spec/integration/folders_messages_spec.rb
@@ -3,7 +3,7 @@ require 'contextio'
 describe "A MessageCollection created from a Folder" do
   let(:api) { double(:api) }
   let(:source) { double(:source, account: 'account') }
-  let(:folder) { ContextIO::Folder.new(api, source: source) }
+  let(:folder) { ContextIO::Folder.new(api, source: source, name: 'folder-name') }
   let(:message_collection) { folder.messages }
 
   it "has a handle on the right account" do
